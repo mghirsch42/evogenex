9423785b1f0af1d5170d969f819b8e683d8adf32
mghirsch42 <hirschmg42@gmail.com>
2023-02-21 16:20:04 -0500

    Add positive/negative to enrichment analysis

--- eval2/ora/exp_ora.R
+++ eval2/ora/exp_ora.R
@@ -4,8 +4,9 @@ library(clusterProfiler)
 source("eval2/ora/ora_functions.R")
 
 method <- "kegg" # go or kegg
-save_file <- "results/tpm_allrep2/orig/gene_lists/adpt_3_10_14/ora_kegg.csv"
-results_file <- "results/tpm_allrep2/orig/gene_lists/adpt_3_10_14/all_results.csv"
+group <- "positive" # positive, negative, or all
+save_file <- "results/tpm_allrep2/orig/gene_lists/adpt_agg/enrichment/ora_kegg_positive.csv"
+results_file <- "results/tpm_allrep2/orig/gene_lists/adpt_agg/all_results.csv"
 results <- read.csv(results_file)
 
 query_genes <- (
@@ -17,6 +18,12 @@ ref_genes <- (
     results
     %>% separate(ensemble_id, c("ensemble_id", NA))
 )
+
+if (group == "positive") {
+    query_genes <- query_genes %>% filter(logFC > 0)
+} else if (group == "negative") {
+    query_genes <- query_genes %>% filter(logFC < 0)
+}
 
 if (method == "go") {
     go_enrich(query_genes, ref_genes, save_file)
