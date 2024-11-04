931cf5915fee885cedaa9883fe92f033ddd382c5
mghirsch42 <hirschmg42@gmail.com>
2024-11-04 16:19:16 -0500

    Updates from reviewers

--- eval_scripts/simulation/get_de_results.py
+++ eval_scripts/simulation/get_de_results.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 import argparse
 
-def main(result_path, save_path):
+def main(sim_dist, result_path, save_path):
     results_df = pd.DataFrame()
     sim = result_path.split("/")[-2]
 
@@ -11,7 +11,7 @@ def main(result_path, save_path):
     for f in [f for f in os.listdir(result_path) if ".csv" in f]:
         temp_df = pd.read_csv(result_path+f)
         temp_df["sim"] = f[:-4]
-        results_df = results_df.append(temp_df, ignore_index=True)
+        results_df = pd.concat([results_df, temp_df], ignore_index=True)
     results_df = results_df.drop("ensemble_id", axis=1)
     print(results_df)
     results_df["de"] = (results_df["fdr_p"] < 0.05).astype(int)
@@ -24,26 +24,38 @@ def main(result_path, save_path):
     print(sim_params)
     if sim == "neut_sim":
         count["sq"] = sim_params.str[1]
-        count["r"] = sim_params.str[3]
-        count = count[["sq", "r", "de"]]
+        if sim_dist == "poisson":
+            count = count[["sq", "de"]]
+        else:
+            count["r"] = sim_params.str[3]
+            count = count[["sq", "r", "de"]]
     if sim == "const_sim":
         count["sq"] = sim_params.str[3]
-        count["r"] = sim_params.str[5]
-        count["a"] = sim_params.str[7]
-        count = count[["sq", "r", "a", "de"]]
+        if sim_dist == "poisson":
+            count["a"] = sim_params.str[5]
+            count = count[["sq", "a", "de"]]
+        else: 
+            count["r"] = sim_params.str[5]
+            count["a"] = sim_params.str[7]
+            count = count[["sq", "r", "a", "de"]]
     if sim == "adpt_sim":
         count["theta_ratio"] = sim_params.str[1]
         count["sq"] = sim_params.str[3]
-        count["r"] = sim_params.str[5]
-        count["a"] = sim_params.str[7]
-        count = count[["sq", "r", "a", "theta_ratio", "de"]]
+        if sim_dist == "poisson":
+            count["a"] = sim_params.str[5]
+            count = count[["sq", "a", "theta_ratio", "de"]]
+        else: 
+            count["r"] = sim_params.str[5]
+            count["a"] = sim_params.str[7]
+            count = count[["sq", "r", "a", "theta_ratio", "de"]]
     count.to_csv(save_path+sim+"_de.csv", index=False)
     print(count)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--result_path", type=str, action="store", default="results/simulated/differential_expression/neut_sim/")
-    parser.add_argument("-s", "--save_path", type=str, action="store", default="results/simulated/summaries/")
+    parser.add_argument("-d", "--sim_dist", type=str, action="store", default="poisson")
+    parser.add_argument("--result_path", type=str, action="store", default="results/poisson_sim/neut_sim_de/")
+    parser.add_argument("-s", "--save_path", type=str, action="store", default="results/poisson_sim/")
     args = parser.parse_args()
 
-    main(args.result_path, args.save_path)
+    main(args.sim_dist, args.result_path, args.save_path)
---
+++ figure_scripts/figure3b_plot_sim.py
@@ -0,0 +1,65 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pandas as pd
+from scipy import stats
+
+##########
+## Figure 3b
+## Plots boxplots of the true positive and false positive predictons
+## of EvoGeneX and differental expression analysis on simulated data
+## across all parameter combinations
+##########
+
+sim_dist = "poisson" # poisson or nb
+save_path = "figures/simulation/{}/".format(sim_dist)
+adpt_sim_adpt_run = pd.read_csv("results/{}_sim/adpt_sim_adpt_run.csv".format(sim_dist))
+adpt_sim_de = pd.read_csv("results/{}_sim/adpt_sim_de.csv".format(sim_dist))
+neut_sim_adpt_run = pd.read_csv("results/{}_sim/neut_sim_adpt_run.csv".format(sim_dist))
+neut_sim_de = pd.read_csv("results/{}_sim/neut_sim_de.csv".format(sim_dist))
+
+if sim_dist == "poisson":
+    adpt_vars = ["sigma_sq", "alpha", "theta_ratio"]
+    neut_vars = ["sigma_sq"]
+else:
+    adpt_vars = ["sigma_sq", "r", "alpha", "theta_ratio"]
+    neut_vars = ["sigma_sq", "r"]    
+
+adpt_sim_adpt_run.columns = adpt_vars + ["# predicted adaptive"]
+neut_sim_adpt_run.columns = neut_vars + ["# predicted adaptive"]
+adpt_sim_de.columns = adpt_vars + ["de"]
+neut_sim_de.columns = neut_vars + ["de"]
+
+print(neut_sim_adpt_run)
+print(len(adpt_sim_adpt_run))
+print(adpt_sim_adpt_run["# predicted adaptive"].sum())
+print(adpt_sim_de["de"].sum())
+print(len(neut_sim_adpt_run))
+print(neut_sim_adpt_run["# predicted adaptive"].sum())
+print(neut_sim_de["de"].sum())
+
+res = stats.ttest_ind(adpt_sim_adpt_run["# predicted adaptive"], adpt_sim_de["de"])
+print("adpt sim t-test:", res)
+res = stats.ttest_ind(neut_sim_adpt_run["# predicted adaptive"], neut_sim_de["de"])
+print("neut sim t-test:", res)
+
+adpt_sim = adpt_sim_adpt_run.merge(adpt_sim_de, on=adpt_vars)
+adpt_sim = pd.melt(adpt_sim, id_vars=adpt_vars, var_name="pred_type", value_name="count", ignore_index=True)
+# print(adpt_sim)
+
+neut_sim = neut_sim_adpt_run.merge(neut_sim_de, on=neut_vars)
+neut_sim = pd.melt(neut_sim, id_vars=neut_vars, var_name="pred_type", value_name="count", ignore_index=True)
+# print(neut_sim)
+
+plt.figure()
+sns.boxplot(data=adpt_sim, x="pred_type", y="count", hue="pred_type")
+plt.title("Adaptively simulated data - true positives")
+# plt.savefig("{}true_positives.svg".format(save_path))
+plt.show()
+plt.close()
+
+plt.figure()
+sns.boxplot(data=neut_sim, x="pred_type", y="count", hue="pred_type")
+plt.title("Neutrally simulated data - false positives")
+# plt.savefig("{}false_positives.svg")
+plt.show()
+plt.close()
